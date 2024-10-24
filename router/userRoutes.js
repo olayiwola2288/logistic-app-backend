d3a379716fbd5e51af7ddb8bb7680158c08376dc
olayiwola2288 <130563088+olayiwola2288@users.noreply.github.com>
2024-10-24 16:47:50 +0100

    Update userRoutes.js

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -6,12 +6,14 @@ const {
   getUsers,
   getUserById,
   updateUser,
+  getMe
 } = require("../controllers/userController");
 
 router.post("/signin", signInUser);
 router.post("/signup", signUpUser);
 
 router.use(verifyToken);
+router.get("/me", getMe)
 router.get("/", getUsers);
 router.get("/:id", getUserById);
 router.put("/:id", updateUser);
